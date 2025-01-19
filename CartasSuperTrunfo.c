954253c4cbec7ee1144af5cb0379188368e92b11
AstroCodingsoftware <astrobertioga.oficial@gmail.com>
2025-01-19 23:02:04 +0000

    cadastro de cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,48 +2,22 @@
 
 int main() {
 
-    //[ Variaveis 👨‍💻 ]//
+    int population, tourist; 
+    float zone, PIB;
 
-    int codeCity;
-    int population;
-    int touristPlaces;
-    int area;
-    float PIB;
-    char (nameCity) [20] = "";
+    printf("==========>> Cadastro de cartas <<============\n\n");
+//entrada de dados
+    printf("Coloque a população: ");
+        scanf("%d", &population);
+    printf("Coloque os pontos turisticos: ");
+        scanf("%d", &tourist);
+    printf("Coloque o PIB: ");
+        scanf("%f", &PIB);
+    printf("Coloque sua Zone: ");
+        scanf("%f", &zone);
 
-    // [ ENTRADA DE DADOS 💻 ] //
-
-    printf("Codigo da cidade: \n");
-    scanf("%d", &codeCity); // pega o codigo da cidade.
-
-    printf("Quantidade de pessoas que vivem nessa cidade?\n:");
-    scanf("%d", &population); // Pega o total de pessoas que vivem nessa cidade.
-
-    printf("Quantas areas turisticas existem nessa cidade?\n:");
-    scanf("%d", &touristPlaces); // pega todas as areas turisticas.
-
-    printf("Qual e o PIB dessa cidade?\n:");
-    scanf("%f", &PIB); // pede o pib dessa cidade.
-
-    printf("Tamanho da area que essa cidade cobre?:\n");
-    scanf("%d", &area); // pede o tamanho que a cidade cobre.
-
-    printf("Diga-me o nome de sua cidade:");
-    scanf("%20s", nameCity); // pede o nome da cidade.
-
-    // [ SAIDA DE DADOS 💻 ] // 
-
-    printf("SUCCESS 🎉🎉 | AQUI ESTÃO OS DADOS INSERIDOS! 💻\n\n");
-
-    /*  Esta printando os dados ao jogador */
-
-    printf("Codigo da cidade: %d\n", codeCity); 
-    printf("População: %d\n", population);
-    printf("Areas Turisticas: %d\n", touristPlaces);
-    printf("PIB: %f\n", PIB);
-    printf("Area ocupada: %d\n", area);
-    printf("Nome da cidade: %s\n", nameCity);                
-
+    // saida de dados
+    printf("Seus dados são!\n\n");
+    printf("População:%d\nTuristicos:%d\nZona:%f\nPIB:%f\n", population, tourist, zone, PIB);
     return 0;
-
 };
